26ba131028307a35d42da8d3fc79d205b251a0d0
Vy Phan <vyphan.cnp@gmail.com>
2018-09-14 22:17:56 -0700

    I command you to work.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+// Greatest Common Denominator
+// PRE: a is a positive integer
+// PRE: b is a positive integer
+// POST: a and b are unchanged
+// RETURN: the greatest common denominator of a and b
+int gcd(const int a, const int b){
+    if(a%b == 0){
+        return b;
+    }else{
+        return gcd(b, a%b);
+    }
+}
+
+
 int main() {
-    std::cout << "Hello, World 1!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
