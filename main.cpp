5dd01e796c465d02db7f292d92693fa7bc16d9a6
Vy Phan <vyphan.cnp@gmail.com>
2018-09-14 22:19:54 -0700

    I command you to work.

--- main.cpp
+++ main.cpp
@@ -16,5 +16,6 @@ int gcd(const int a, const int b){
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "The greatest common denominator of 30 and 18: " << gcd(30,18) << std::endl;
     return 0;
 }
